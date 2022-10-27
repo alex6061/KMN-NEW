14211892a5027b1fcdd3b527a02ff893a5687d33
alex6061 <39526012+alex6061@users.noreply.github.com>
2022-10-27 18:25:23 +0300

    commit

---
+++ itc-slider.js
@@ -0,0 +1,28 @@
+// new ItcSlider('.slider');
+//
+//
+// document.addEventListener('DOMContentLoaded', () => {
+//     new ItcSlider('.slider');
+// });
+//
+// document.addEventListener('DOMContentLoaded', () => {
+//     document.querySelectorAll('.slider').forEach((el) => {
+//         new ItcSlider(el);
+//     });
+// });
+//
+// this._elsItem.forEach((el, index) => {
+//     el.dataset.index = index;
+//     el.dataset.order = index;
+//     el.dataset.translate = 0;
+// });
+//
+// this._el.addEventListener('mouseenter', () => {
+//     this._autoplay('stop');
+// });
+// this._el.addEventListener('mouseleave', () => {
+//     this._autoplay();
+// });
+//
+//
+// console.log ("hellow")
